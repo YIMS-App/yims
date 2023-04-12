e6ecb0169a8339670dc5ffc7e6af6be6b08133a3
kellyq17 <55288693+kellyq17@users.noreply.github.com>
2023-04-11 20:09:10 -0400

    Backend (#29)
    * add getcollegeparticipation route
    
    * remove print from college participation test
    
    * update match_info and bets route
    
    ---------
    
    Co-authored-by: Mary Jiang <mary.jiang2020@gmail.com>

--- flask_server/app.py
+++ flask_server/app.py
@@ -433,7 +433,7 @@ def updatebet():
         matchid = data['matchid']
         pointsbet = data['pointsbet']
         winner = data['winner']
-        exists = data['exists'] # see if a bet exists already
+        exists = query_db(queries.bet_earnings(), [netid, matchid], database_url=app.config['DATABASE'])[0][0] # see if a bet exists already
 
         # check if the user has enough money to make a bet
         money = query_db(queries.user_participation_points(), [netid], database_url=app.config['DATABASE'])[0][0]
@@ -476,13 +476,23 @@ def getparticipationmatch():
 
         params = [netid, matchid]
         result = query_db(queries.user_match_attended(), params, database_url=app.config['DATABASE'])
-        
-        if result:
-                output = jsonify_rows(result)[0]
-        else:
-            output = jsonify({'success': False})
+        output = jsonify_rows(result)[0]
             
     except Exception as ex:
         print(ex)
         return jsonify(error=404, text=str(ex)), 404
+    return output, 200
+
+@app.route("/getcollegeparticipation", methods=["GET"])
+def getcollegeparticipation():
+    """
+    Gets total participation scores for all colleges
+    """
+    output = None
+    try:
+        result = query_db(queries.college_participation_score(), [CURR_YEAR], database_url=app.config['DATABASE'])
+        output = jsonify({'scores': jsonify_rows(result)})
+    except Exception as ex:
+        print(ex)
+        return jsonify(error=404, text=str(e)), 404
     return output, 200
--- flask_server/test/test_integration.py
+++ flask_server/test/test_integration.py
@@ -52,15 +52,15 @@ def test_matchinfo():
     r = requests.post(url = TEST_ADDRESS + '/matchinfo', json = data) 
     output = r.json()
 
-    assert output['matchid'] == 1
-    assert output['id1'] == 1
-    assert output['id2'] == 2
+    assert output['college1'] == "Benjamin Franklin"
+    assert output['college1Abbrev'] == "BF"
+    assert output['college2'] == "Branford"
+    assert output['college2Abbrev'] == "BR"
     assert output['sport'] == "soccer"
     assert output['location'] == "school"
-    assert output['startTime'] == "2022-03-08 12:34:29"
-    assert output['endTime'] == "2022-03-08 12:35:29"
-    assert output['winner'] == 1
-    assert output['manager'] == "ey229"
+    assert output['startTime'] == "2007-05-08 12:34:29"
+    assert output['endTime'] == "2007-05-08 12:35:29"
+    assert output['winner'] == "Benjamin Franklin"
 
 def test_totalscores():
     
@@ -219,3 +219,11 @@ def test_getparticipationmatch():
     output = r.json()
 
     assert output['status'] == 2
+
+def test_collegeparticipation():
+    
+    r = requests.get(url = TEST_ADDRESS + '/getcollegeparticipation')
+    output = r.json()
+
+    assert output['scores'][0]["college"] == "Grace Hopper"
+    assert output['scores'][0]["score"] == 90.0
