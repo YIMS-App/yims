05ee7f18e235003573f21e556f0a0911bfcca846
Mary Jiang <mary.jiang2020@gmail.com>
2023-04-17 19:24:00 -0400

    fix jest tests

--- app/components/shared/__tests__/ModalDropdown.test.js
+++ app/components/shared/__tests__/ModalDropdown.test.js
@@ -1,5 +1,5 @@
 import {render, screen, cleanup} from '@testing-library/react-native';
-import { ModalDropdown } from '../ModalDropdown';
+import ModalDropdown from '../ModalDropdown';
 
 afterEach(() => {
     cleanup();
