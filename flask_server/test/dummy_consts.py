3fcc6ac4c27e0bc31e0f9941b31afdd1f8bb9994
Mary Jiang <mary.jiang2020@gmail.com>
2023-04-17 21:45:09 -0400

    update sports constants for dummy data

--- flask_server/test/dummy_consts.py
+++ flask_server/test/dummy_consts.py
@@ -5,7 +5,10 @@ ADMINS = {
     "ey229", "ag2658", "bmv6", "cmo48", "awx2", "kq44", "mj598"
 }
 FILENAME = 'test/dummytable'
-sports = {'soccer': (11, "⚽"), 'dodgeball': (6, "🪩"), 'volleyball': (6, "🏐")}
+sports = {'soccer': (11, "⚽"), 'flag football': (6, "🏈"), 'spikeball': (6, "🦔"), 
+        'cornhole': (6, "🌽"), 'pickleball': (6, "🥒"), 'ping pong': (10, "🏓"),
+        'basketball': (5, "🏀"), 'dodgeball': (8, "🤾"), 'soccer (indoors)': (5, "🥅"),
+        'volleyball': (6, "🏐"), 'broomball': (6, "🧹"), 'water polo': (6, "🤽")}
 
 colleges = {
     'BF': "Benjamin Franklin", 
